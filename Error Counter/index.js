2bfbbf67eda32a978f865f2e17a4d86a5ba87fac
dharsan19 <idappadidharsan@gmail.com>
2022-08-15 01:22:06 +0530

    Feat: Error Counter app added

---
+++ Error Counter/index.js
@@ -0,0 +1,15 @@
+let countpeople = document.getElementById("countpeople")
+let saveel = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countpeople.innerText = count
+}
+
+function save() {
+    let countstr = count + " - "
+    saveel.textContent += countstr
+    count = 0
+    countpeople.textContent = count
+}
